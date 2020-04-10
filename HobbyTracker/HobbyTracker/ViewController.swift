f8c08fa203f07f50a44bd57a20bbd5a399e88b1f
Ian French <igf2011@hotmail.com>
2020-04-09 21:40:17 -0400

    Initital commit

---
+++ HobbyTracker/HobbyTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HobbyTracker
+//
+//  Created by Ian French on 4/9/20.
+//  Copyright © 2020 Ian French. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
